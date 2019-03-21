c6622349e25e5352e644d9a595d8acda1c936b7c
Troy Gooden <tro12185@gmail.com>
2019-03-21 02:23:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees.collect do |x|
+    badge_maker(x)
+  end 
+end 
+
+def assign_rooms(attendees)
+  attendees.collect do |name|
+    name.each_with_index do |name, index|
+      "Hello, #{name}! You'll be assigned to room #{index + 1}"
+    end 
+  end 
+end 
