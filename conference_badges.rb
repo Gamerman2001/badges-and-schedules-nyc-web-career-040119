57fb71a5e580ae5f6f125c930d589959b006ed9b
Troy Gooden <tro12185@gmail.com>
2019-03-21 03:01:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,11 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-  puts 
-  end  
+  batch_badge_creator(attendees).each do |name|
+      puts name
+  end
+  assign_rooms(attendees).each do |name|
+      puts name
+  end
+  
 end 
